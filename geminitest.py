bbb32c2f665c49b1bb834107f1a11e0c93f45d84
PS1353 <128229111+PS1353@users.noreply.github.com>
2024-07-04 06:50:03 +0530

    Update geminitest.py

--- geminitest.py
+++ geminitest.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-api_key = 'AIzaSyCkHD6jP6ahN4HaV2oIqG2Q1-aAsLNCuc0'
+api_key = 'your api key'
 
 def gemini(prompt,parameters):
     genai.configure(api_key=api_key)
